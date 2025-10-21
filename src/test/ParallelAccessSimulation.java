e06f2e4629c4027eb84eb81ff01defb092f62ff3
Salexis55 <a.garciaperez@colegiovirgendeeuropa.edu.es>
2025-10-21 11:05:46 +0200

    JDBCtest 21/10

--- src/test/ParallelAccessSimulation.java
+++ src/test/ParallelAccessSimulation.java
@@ -1,23 +1,30 @@
+/**
+ * 
+ */
 package test;
 
 import java.util.ArrayList;
 
 import user.UserDAO;
-
+/**
+ * @author  10 oct 2025
+ */
 public class ParallelAccessSimulation {
 	public static void main(String[] args) {
 		UserDAO userDao = new UserDAO();
-		long fromUserId = 10;
-		long toUserId = 20;
+		long fromUserId = 2;
+		long toUserId = 1;
 		ArrayList<Thread> threads = new ArrayList<Thread>(100);
 		System.out.println(threads.size());
 		for (int i = 0; i < 100; i++) {
 			Thread thread = new Thread(() -> {
 				userDao.transfer(fromUserId, toUserId, 1);
-			}
-			);
+			});
 			threads.add(thread);
 		}
-		
+		for (Thread thread : threads) {
+			thread.start();
+			
+		}
 	}
 }
